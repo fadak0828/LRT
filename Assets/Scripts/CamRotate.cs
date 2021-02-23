3df331395cb6ee7757a21cb5f5d608c783cf61f2
rlatjdgh9612 <78453933+rlatjdgh9612@users.noreply.github.com>
2021-02-23 11:37:51 +0900

    playerScene 임포트
    playerScene 임포트

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 마우스 입력에 따라 Player 1인칭 시점에서 카메라를 회전하고 싶다
+public class CamRotate : MonoBehaviour
+{
+    float rx, ry;
+    public float rotspeed = 200;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        // 마우스 입력에 따라
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+        rx += my * rotspeed * Time.deltaTime;
+        ry += mx * rotspeed * Time.deltaTime;
+
+        // 카메라를 회전하고 싶다
+        rx = Mathf.Clamp(rx, -70, 70);
+        transform.eulerAngles = new Vector3(-rx, ry, 0);
+    }
+}
