32eb365641ee2aa823e39ea82797eb219c230ffd
rlatjdgh9612 <78453933+rlatjdgh9612@users.noreply.github.com>
2021-02-26 17:37:02 +0900

    1. Player Grab 및 Drop 구현 - 완료 2. Grip 스크립트 수정 - 완료

---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Grabbable : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    internal void 잡다(Vector3 pos, Transform parent)
+    {
+        // 만약 다른손이 잡고있던 물체였다면 다른손에게 "놔줘" 라고 요청하고싶다
+        if (transform.parent != null)
+        {
+            transform.parent.GetComponent<Grip>().놔줘();
+            transform.parent = null;
+        }
+        transform.position = pos;
+        transform.parent = parent;
+        GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+
+    internal void 놓다(Valve.VR.SteamVR_Behaviour_Pose pose)
+    {
+        transform.parent = null;
+    }
+}
