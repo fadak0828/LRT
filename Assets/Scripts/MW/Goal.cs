d61742f5c5e0f767e52fa0268622a3d5f679890d
fadak <asd0828@gmail.com>
2021-02-28 21:46:45 +0900

    Goal 프리팹 수정(색깔 및 미니 마커 자동 변경) & 씬에 적용

--- Assets/Scripts/MW/Goal.cs
+++ Assets/Scripts/MW/Goal.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Goal : MonoBehaviour, LaserInput
 {
     public LaserColor needColor;
+    public GameObject needColorObj;
+    public GameObject redMarker;
+    public GameObject blueMarker;
+    public GameObject greenMarker;
     public bool goalIn;
+
+    private void Awake() {
+        redMarker.SetActive(((int)needColor & (int)LaserColor.RED) != 0);    
+        blueMarker.SetActive(((int)needColor & (int)LaserColor.BLUE) != 0);    
+        greenMarker.SetActive(((int)needColor & (int)LaserColor.GREEN) != 0);
+
+        Shader shader = Shader.Find("Standard");
+        Shader.EnableKeyword("_EMISSION");
+        
+        Material mat = new Material(shader);
+        mat.SetColor("_EmissionColor", Laser.GetColor(needColor));
+
+        needColorObj.GetComponent<Renderer>().sharedMaterial = mat;
+    }
 
     public void OnLaserInput(LaserHit hit) {
         goalIn = hit.color == needColor;
