4aad2aad5442587281d0267a0c3cd79ed255acaa
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2021-03-04 16:36:15 +0900

    goal 점점 밝아지기 & 터지는 효과 & 벽 넘기는 효과

--- Assets/Scripts/MW/Goal.cs
+++ Assets/Scripts/MW/Goal.cs
@@ -10,10 +10,13 @@ public class Goal : LaserInput
     public GameObject redMarker;
     public GameObject blueMarker;
     public GameObject greenMarker;
+    public GameObject particleFacotry;
 
     private Color originEmissionColor;
     private Color emissionColor;
     public bool _goalIn;
+    private Coroutine brightCoroutine;
+
     public bool goalIn
     {
         get { return _goalIn; }
@@ -56,15 +59,45 @@ public class Goal : LaserInput
 
         originEmissionColor = GetComponent<Renderer>().material.GetColor("_EmissionColor");
         goalIn = false;
+
+        
     }
 
     private void EmissionOn()
     {
-        GetComponent<Renderer>().material.SetColor("_EmissionColor", Laser.GetColor(needColor) * 0.8f);
-    }
+        /////
 
+        brightCoroutine = StartCoroutine(Bright());       
+        //////
+    }
+    //인보크 사용해서 점점 밝게 하기
+    IEnumerator Bright() {
+        int count = 0;
+        float bright = 0;
+
+        while (count < 15) {
+            bright += 0.08f;
+            count++;
+            GetComponent<Renderer>().material.SetColor("_EmissionColor", Laser.GetColor(needColor) * bright);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+   public void OnStageClear() {
+        GameObject particle = Instantiate(particleFacotry);
+        particle.transform.position = transform.position;
+       // particle.transform.eulerAngles = new Vector3(180, 0, 0);
+        particle.GetComponent<ParticleSystem>().Stop();
+        particle.GetComponent<ParticleSystem>().Play();
+        Destroy(particle, 1);
+        gameObject.SetActive(false);
+    }
     private void EmissionOff()
     {
+        if (brightCoroutine != null) {
+            StopCoroutine(brightCoroutine);
+            brightCoroutine = null;
+        }
         GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
     }
 
