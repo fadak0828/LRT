506ae19a953c3ca0f91436f12bdecc9fb1aaadd5
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2021-03-05 13:11:25 +0900

    Revert "Merge remote-tracking branch 'origin/feature/player' into feature/environment"
    This reverts commit ac0e9261c6fe90e549e1bddf2972cb97e6fe2fd4.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,9 +6,8 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
 
-
     public void PlayGame() {
-        Fader.instance.FadeOut(1);
+
         SceneManager.LoadScene("Background");
     }
 
